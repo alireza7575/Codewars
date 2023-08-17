5e59d22805b6df5cda5c697fe99006112d474902
AlirezaAhmadi <ahmadyalireza75@gmail.com>
2023-08-17 15:15:46 +0200

    implement Sudoku kata.
    https://www.codewars.com/kata/540afbe2dc9f615d5e000425

---
+++ C#/Codewars.Tests/SudokuTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Tests;
+
+public sealed class SudokuTests
+{
+	[Test]
+	public void Correct9By9Sudoku()
+	{
+		var solvedSudoku = new Sudoku(new[]
+		{
+			new[] { 5, 3, 4, 6, 7, 8, 9, 1, 2 }, new[] { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
+			new[] { 1, 9, 8, 3, 4, 2, 5, 6, 7 }, new[] { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
+			new[] { 4, 2, 6, 8, 5, 3, 7, 9, 1 }, new[] { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+			new[] { 9, 6, 1, 5, 3, 7, 2, 8, 4 }, new[] { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
+			new[] { 3, 4, 5, 2, 8, 6, 1, 7, 9 }
+		});
+		Assert.That(solvedSudoku.IsValid(), Is.True);
+	}
+
+	[Test]
+	public void Wrong2By2Sudoku()
+	{
+		var solvedSudoku = new Sudoku(new[] { new[] { 1, 2 }, new[] { 1, 2 } });
+		Assert.That(solvedSudoku.IsValid(), Is.False);
+	}
+
+	[Test]
+	public void Correct4By4Sudoku()
+	{
+		var solvedSudoku = new Sudoku(new[]
+		{
+			new[] { 1, 4, 2, 3 }, new[] { 3, 2, 4, 1 }, new[] { 4, 1, 3, 2 }, new[] { 2, 3, 1, 4 }
+		});
+		Assert.That(solvedSudoku.IsValid(), Is.False);
+	}
+
+	[Test]
+	public void Wrong4By4Sudoku()
+	{
+		var invalidSudoku = new Sudoku(new[]
+		{
+			new[] { 1, 4, 2, 3 }, new[] { 3, 2, 4, 1 }, new[] { 4, 2, 3, 2 }, new[] { 2, 3, 1, 4 }
+		});
+		Assert.That(invalidSudoku.IsValid(), Is.False);
+	}
+
+	[Test]
+	public void Wrong9By9Sudoku()
+	{
+		var invalidSudoku = new Sudoku(new[]
+		{
+			new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+			new[] { 2, 3, 1, 5, 6, 4, 8, 9, 7 },
+			new[] { 3, 1, 2, 6, 4, 5, 9, 7, 8 },
+			new[] { 4, 5, 6, 7, 8, 9, 1, 2, 3 },
+			new[] { 5, 6, 4, 8, 9, 7, 2, 3, 1 },
+			new[] { 6, 4, 5, 9, 7, 8, 3, 1, 2 },
+			new[] { 7, 8, 9, 1, 2, 3, 4, 5, 6 },
+			new[] { 8, 9, 7, 2, 3, 1, 5, 6, 4 },
+			new[] { 9, 7, 8, 3, 1, 2, 6, 4, 5 }
+		});
+		Assert.That(invalidSudoku.IsValid(), Is.False);
+	}
+
+	[Test]
+	public void NotComplicated()
+	{
+		var invalidSudoku = new Sudoku(new[]
+		{
+			new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 }, new[] { 1 }
+		});
+		Assert.That(invalidSudoku.IsValid(), Is.False);
+	}
+}
---
+++ C#/Codewars/Sudoku.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars;
+
+public sealed class Sudoku
+{
+	private readonly int[][] board;
+	public Sudoku(int[][] board) => this.board = board;
+
+	public bool IsValid()
+	{
+		try
+		{
+			return CheckRow() && CheckColumn() && CheckSubGrids();
+		}
+		catch
+		{
+			return false;
+		}
+	}
+
+	private bool CheckColumn()
+	{
+		for (var columnIndex = 0; columnIndex < board.Length; columnIndex++)
+		{
+			var seenNumbers = new List<int>();
+			foreach (var row in board)
+				if (!seenNumbers.Contains(row[columnIndex]) && row[columnIndex] != 0)
+					seenNumbers.Add(row[columnIndex]);
+				else
+					return false;
+		}
+		return true;
+	}
+
+	private bool CheckRow()
+	{
+		foreach (var row in board)
+		{
+			var seenNumbers = new List<int>();
+			for (var columnIndex = 0; columnIndex < board.Length; columnIndex++)
+				if (!seenNumbers.Contains(row[columnIndex]) && row[columnIndex] != 0)
+					seenNumbers.Add(row[columnIndex]);
+				else
+					return false;
+		}
+		return true;
+	}
+
+	private bool CheckSubGrids()
+	{
+		for (var rowIndex = 0; rowIndex < board.Length; rowIndex += 3)
+		for (var columnIndex = 0; columnIndex < board.Length; columnIndex += 3)
+			if (!LookForRepeatedNumber(rowIndex, columnIndex))
+				return false;
+		return true;
+	}
+
+	private bool LookForRepeatedNumber(int rowIndex, int columnIndex)
+	{
+		var seenNumbers = new List<int>();
+		return board.Skip(rowIndex).Take(3).Select(row => row.Skip(columnIndex).Take(3)).
+			SelectMany(subRow => subRow).All(value => CheckHistory(seenNumbers, value));
+	}
+
+	private static bool CheckHistory(ICollection<int> seenNumbers, int value)
+	{
+		if (!seenNumbers.Contains(value))
+			seenNumbers.Add(value);
+		else
+			return false;
+		return true;
+	}
+}
